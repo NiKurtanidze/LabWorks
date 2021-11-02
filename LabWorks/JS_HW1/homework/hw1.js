69b2d8c32538c41e625aff304cba62326eaef5c4
Nino Kurtanidze <nikurtanidze@tbcbank.com.ge>
2021-11-02 22:26:55 +0400

    all together

---
+++ LabWorks/JS_HW1/homework/hw1.js
@@ -0,0 +1,5 @@
+let arr = ['javascript', 'php', 'c#', 'python', 'c++', 'kotlin', 'c', 'swift'];
+
+arr.forEach(element => {
+    if(element.length >= 5) console.log(element)
+});
---
+++ LabWorks/JS_HW1/homework/hw2.js
@@ -0,0 +1,5 @@
+let arr = [2, -3, 5, 11, 1, -30, -11, 100, -1, -3, -4];
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] > 0) console.log(arr[i])
+}
