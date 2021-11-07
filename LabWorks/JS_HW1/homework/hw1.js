bdaba0bc8cb9b21ce87fb9de071b3a57016f7ee8
Nino Kurtanidze <nikurtanidze@tbcbank.com.ge>
2021-11-07 12:33:32 +0400

    first has been changed

--- LabWorks/JS_HW1/homework/hw1.js
+++ LabWorks/JS_HW1/homework/hw1.js
@@ -3,3 +3,10 @@ let arr = ['javascript', 'php', 'c#', 'python', 'c++', 'kotlin', 'c', 'swift'];
 arr.forEach(element => {
     if(element.length >= 5) console.log(element)
 });
+
+console.log()
+console.log()
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i].length >= 5) console.log(arr[i])
+}
--- LabWorks/JS_HW1/homework/hw3.js
+++ LabWorks/JS_HW1/homework/hw3.js
@@ -2,6 +2,7 @@ let arr = [[2, -3, 5, 11], [1, -30, -11, 100], [-1, -3, -4]];
 
 for(let i = 0; i < arr.length; i++){
     for(let j = 0; j < arr[i].length; j++){
+        let element = arr[i][j]
         if(arr[i][j] > 0) console.log(arr[i][j])
     }
 }
